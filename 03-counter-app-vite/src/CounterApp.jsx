45a660cf14255c3390ba95dbd18f16a95d828507
Juan Diego RG <minionpaul85@gmail.com>
2024-04-06 17:19:27 -0500

    Button Counter Hooks

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+
 export const CounterApp = ({value}) => {
+
+  const [ counter, setCounter ] = useState( value );
+
+  const HandleAdd = () => { 
+    setCounter ( counter + 1 );
+  }
+
+  const HandelSubs = () => { 
+    setCounter ( counter - 1 );
+  }
+
+  const HandleReset = () => { 
+    setCounter ( value );
+  }
+  
   return (
     <>
     <h1>CounterApp</h1>
-    <h2> { value } </h2>
+    <h2> { counter } </h2>
+
+    <button onClick={ HandleAdd }> +1 </button>
+    <button onClick={ HandelSubs }> -1 </button>
+    <button onClick={ HandleReset }> Reset </button>
+
     </>
   )
 }
