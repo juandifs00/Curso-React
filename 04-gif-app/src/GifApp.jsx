3d63f674442ae748f3307ef80dd4a79562157967
Juan Diego RG <minionpaul85@gmail.com>
2024-04-11 15:28:00 -0500

    Evento de busqueda

--- 04-gif-app/src/GifApp.jsx
+++ 04-gif-app/src/GifApp.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
+
 export const GifApp = () => {
+
+    const [categories, setCategories] = useState( [ 'Fast & Furious' , 'Disney'] )
+
+    // console.log(categories)
+
+    const onAddCategory = () => {
+
+        setCategories( [ 'Rainbow Six', ...categories ] )
+        
+        // setCategories( cat => [ ...cat, 'Rainbow Six' ] )
+
+    }
+
   return (
     <>
+    {/* Título */}
     <h1>GifApp</h1>
+
+    {/* Input */}
+    <AddCategory setCategories={ setCategories }/>
+
+
+    {/* Listado de Gifs */}
+    
+    <ol>
+        {
+            categories.map( category => {
+                return <li key = { category }> {category} </li>
+            } )
+        }
+    </ol>
+
+        {/* Gif Item */}
+
     </>
   )
 }
---
+++ 04-gif-app/src/components/AddCategory.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export const AddCategory = ( { setCategories } ) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = ({ target }) => {
+        setInputValue( target.value )
+    }
+    
+    const onSubmit = ( event ) => {
+        event.preventDefault()
+        
+        if( inputValue.trim().length <= 1 ) return
+
+        setCategories( categories => [inputValue, ...categories] )
+
+        setInputValue('')
+    }
+
+  return (
+
+    <form onSubmit={ (event) => onSubmit(event) }>
+    
+        <input 
+            type="text" 
+            placeholder="Buscat Gifs"
+            value={ inputValue }
+            onChange={ onInputChange }
+        />
+
+    </form>
+  )
+}
