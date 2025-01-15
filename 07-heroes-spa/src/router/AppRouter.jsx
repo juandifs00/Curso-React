5413596740baabfcadd95b9cebc5491b1a3bed2d
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-01-14 19:36:09 -0500

    Navbar y archivo de barril

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,11 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import MarvelPage from "../heroes/pages/MarvelPage";
+
 import DCPage from "../heroes/pages/DCPage";
+import MarvelPage from "../heroes/pages/MarvelPage";
 import LoginPage from "../auth/pages/LoginPage";
+
+import { Navbar } from "../ui";
 
 const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DCPage />} />
