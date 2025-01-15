37531fe1a36c2d2ae958faf89f0026682a83b90d
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-01-14 21:59:28 -0500

    Rutas protegidas

---
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Navbar } from "../../ui";
+
+import DCPage from "../pages/DCPage";
+import HeroPage from "../pages/HeroPage";
+import MarvelPage from "../pages/MarvelPage";
+import SearchPage from "../pages/SearchPage";
+
+const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <Routes>
+          <Route path="marvel" element={<MarvelPage />} />
+          <Route path="dc" element={<DCPage />} />
+
+          <Route path="search" element={<SearchPage />} />
+          <Route path="hero" element={<HeroPage />} />
+
+          <Route path="/" element={<Navigate to="/marvel" />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
+
+export default HeroesRoutes;
--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,22 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import DCPage from "../heroes/pages/DCPage";
-import MarvelPage from "../heroes/pages/MarvelPage";
 import LoginPage from "../auth/pages/LoginPage";
-
-import { Navbar } from "../ui";
+import HeroesRoutes from "../heroes/routes/HeroesRoutes";
 
 const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DCPage />} />
-
         <Route path="login" element={<LoginPage />} />
-
-        <Route path="/" element={<Navigate to="/marvel" />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
