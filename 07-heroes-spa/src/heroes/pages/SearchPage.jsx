ab86c88819f3a7cd071f16f1dba43e52a14f382d
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-01-17 22:01:29 -0500

    flitrado de heroes y mejora en la visualización

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -2,23 +2,27 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
 import useForm from "../../hooks/useForm";
 import HeroCard from "../components/HeroCard";
+import { getHeroesByName } from "../helpers/getHeroesByName";
 
 const SearchPage = () => {
   const navigate = useNavigate();
-
   const location = useLocation();
 
   const { q = "" } = queryString.parse(location.search);
+  const heroes = getHeroesByName(q);
+
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
 
   const { searchText, onInputChange } = useForm({
-    searchText: "",
+    searchText: q,
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) {
-      return;
-    }
+    // if (searchText.trim().length <= 1) {
+    //   return;
+    // }
 
     navigate(`?q=${searchText}`);
   };
@@ -50,13 +54,33 @@ const SearchPage = () => {
           <h4>Resultados</h4>
           <hr />
 
-          <div className="alert alert-info">Buscar un heroe</div>
+          {/* {q === "" ? (
+            <div className="alert alert-primary">Buscar un heroe</div>
+          ) : (
+            heroes.length === 0 && (
+              <div className="alert alert-danger">
+                No hay resultados para <b>{q}</b>
+              </div>
+            )
+          )} */}
 
-          <div className="alert alert-danger">
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Buscar un heroe
+          </div>
+
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
             No hay resultados para <b>{q}</b>
           </div>
 
-          {/* <HeroCard /> */}
+          {heroes.map((heroe) => (
+            <HeroCard key={heroe.id} {...heroe} />
+          ))}
         </div>
       </div>
     </>
