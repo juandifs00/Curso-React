1e04c395ed4846a02d704944fb1cc47fdbfd174b
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-01-17 21:21:19 -0500

    Componente de búsqueda y queryparams

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,66 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import useForm from "../../hooks/useForm";
+import HeroCard from "../components/HeroCard";
+
 const SearchPage = () => {
-  return <h1>SearchPage</h1>;
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: "",
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) {
+      return;
+    }
+
+    navigate(`?q=${searchText}`);
+  };
+
+  return (
+    <>
+      <h1>Buscar</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Buscar heroe</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="Buscar un heroe"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <button className="btn btn-outline-primary mt-3">Buscar</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+
+          <div className="alert alert-info">Buscar un heroe</div>
+
+          <div className="alert alert-danger">
+            No hay resultados para <b>{q}</b>
+          </div>
+
+          {/* <HeroCard /> */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
