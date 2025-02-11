a0588e74d3b9da2162df2cda3e47912ea310b0c4
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-10 22:56:26 -0500

    Test del Navbar con el mock del useNavigate

---
+++ 07-heroes-spa/tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,51 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../src/auth/context/Authcontext";
+import { Navbar } from "../../../src/ui/components/Navbar";
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUseNavigate,
+}));
+
+describe("Pruebas en el componente <Navbar/>", () => {
+  const contextValue = {
+    logged: true,
+    username: {
+      name: "Juan",
+    },
+    logout: jest.fn(),
+  };
+
+  beforeEach(() => jest.clearAllMocks());
+
+  test("Debe de mostar el nombre del usuario loggeado", () => {
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    expect(screen.getByText("Juan")).toBeTruthy();
+  });
+
+  test("Debe de llamar el botón de logout y navigate cuando se hace click en el botón", () => {
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    const logoutBtn = screen.getByRole("button");
+    fireEvent.click(logoutBtn);
+
+    expect(contextValue.logout).toHaveBeenCalled();
+    expect(mockedUseNavigate).toHaveBeenCalledWith("/login", { replace: true });
+  });
+});
