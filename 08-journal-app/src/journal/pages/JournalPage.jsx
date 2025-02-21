5dfe349ee4ca41c127fb44f1f7a0724fb1150073
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-21 18:51:57 -0500

    Boton flotante

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
+import { IconButton } from "@mui/material";
 import JournalLayout from "../layout/JournalLayout";
 import NoteView from "../views/NoteView";
 import NothingSelectedView from "../views/NothingSelectedView";
+import { AddOutlined } from "@mui/icons-material";
 
 const JournalPage = () => {
   return (
@@ -16,12 +18,25 @@ const JournalPage = () => {
         nostrud sit ullamco.
       </Typography> */}
 
-      {/* <NothingSelectedView /> */}
-
       {/* NothingSelected */}
-      <NoteView />
+      <NothingSelectedView />
 
+      {/* <NoteView /> */}
       {/* NoteView */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
