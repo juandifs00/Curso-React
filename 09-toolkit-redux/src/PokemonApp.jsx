54b6db25f4977992b1d216bc339093b5381fd6c8
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-24 23:17:31 -0500

    Redux de pokemons

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+
+const PokemonApp = () => {
+  return (
+    <>
+    <h1>PokemonApp</h1>
+    <hr />
+    <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+        <li>Hola</li>
+    </ul>
+    </>
+  )
+}
+
+export default PokemonApp
--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./store/store.js";
 import "./index.css";
+import PokemonApp from "./PokemonApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </StrictMode>
 );
