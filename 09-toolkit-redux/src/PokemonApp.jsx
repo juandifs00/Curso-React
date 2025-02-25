47d1f1c0a35fe1a93c8ee7ce440f93d72f03d552
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-25 00:27:14 -0500

    Mapeo de los pokemons

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemons/thunks";
 
 const PokemonApp = () => {
   const dispatch = useDispatch();
+
+  const {
+    isLoading,
+    pokemons = [],
+    page,
+  } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
     dispatch(getPokemons());
@@ -13,11 +19,16 @@ const PokemonApp = () => {
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>Loading: {isLoading ? "true" : "false"} </span>
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
       </ul>
+
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
     </>
   );
 };
