d0a00d6e454fae34af90e6fa76e5b47813f666a0
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-25 00:13:58 -0500

    Petición HTTP con axios

--- 09-toolkit-redux/src/store/slices/pokemons/thunks.js
+++ 09-toolkit-redux/src/store/slices/pokemons/thunks.js
@@ -1,10 +1,20 @@
+import { pokemonApi } from "../../../api/pokemonAPi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page = 0) => {
-  return async(dispatch, getState) => {
+  return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
     // TODO: Realizar petición HTTP
-    // dispatch(setPokemons());
+    // const response = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // );
+    // const data = await response.json();
+
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
+    );
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
