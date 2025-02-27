2b97d18c4323e4228ef30b95cf5e46617f7da187
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-26 23:51:38 -0500

    Consumo del API con el custom hook

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,12 +1,25 @@
+import { useGetTodosQuery } from "./store/apis/todosApi";
+
 const TodoApp = () => {
+  const { data: todos, isLoading } = useGetTodosQuery();
+
   return (
     <>
       <h1>Todos - RTK - Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading... : {isLoading ? "true" : "false"}</h4>
 
       <pre>...</pre>
-      
+
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>
+            <strong> {todo.completed ? "DONE" : "PENDING"} </strong>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+
       <button>Next Todo</button>
     </>
   );
