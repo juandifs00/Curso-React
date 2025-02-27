b139d6b9266f7a52e053a0687fd5c2f9961c9034
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-02-27 17:50:20 -0500

    Instalación y primeras configuraciones Redux en JournalApp

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import JournalApp from "./JournalApp.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true }}>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter future={{ v7_startTransition: true }}>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
