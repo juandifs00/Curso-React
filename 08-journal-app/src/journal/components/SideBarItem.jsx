1cb3084699958481c4becc7dba9ce308cee9bbca
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-03-12 18:32:08 -0500

    Activar la nota y mostrarla

--- 08-journal-app/src/journal/components/SideBarItem.jsx
+++ 08-journal-app/src/journal/components/SideBarItem.jsx
@@ -1,4 +1,5 @@
-import { TurnedInNot } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { useMemo } from "react";
 import {
   Grid,
   ListItem,
@@ -6,16 +7,23 @@ import {
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
-import { useMemo } from "react";
+import { setActiveNote } from "../../store/journal/journalSlice";
+import { TurnedInNot } from "@mui/icons-material";
 
-const SideBarItem = ({ title = "", body, id }) => {
+const SideBarItem = ({ title = "", body, id, date, imageUrls = [] }) => {
+  const dispatch = useDispatch();
+
+  const onClickNote = () => {
+    dispatch(setActiveNote({ title, body, id, date, imageUrls }));
+  };
+
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
 
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
