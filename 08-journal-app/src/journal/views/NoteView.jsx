285c10ea2b53f21fedcde7cd0f4a9be6507ba98c
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2025-03-12 19:00:22 -0500

    Mostrar los datos de la nota seleccionada

--- 08-journal-app/src/journal/views/NoteView.jsx
+++ 08-journal-app/src/journal/views/NoteView.jsx
@@ -1,8 +1,21 @@
 import { SaveOutlined } from "@mui/icons-material";
 import { Button, Grid, TextField, Typography } from "@mui/material";
 import ImageGallery from "../components/ImageGallery";
+import useForm from "../../hooks/useForm";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 const NoteView = () => {
+  const { active: note } = useSelector((state) => state.journal);
+
+  const { body, title, date, onInputChange, formState } = useForm(note);
+
+  const dateString = useMemo(() => {
+    const newDate = new Date(date);
+
+    return newDate.toUTCString();
+  }, [date]);
+
   return (
     <Grid
       container
@@ -14,7 +27,7 @@ const NoteView = () => {
     >
       <Grid item>
         <Typography fontSize={39} fontWeight="light">
-          20 Febrero, 2025
+          {dateString}
         </Typography>
       </Grid>
 
@@ -33,6 +46,9 @@ const NoteView = () => {
           placeholder="Ingrese un título"
           label="Título"
           sx={{ border: "none", mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
         />
 
         <TextField
@@ -42,6 +58,9 @@ const NoteView = () => {
           multiline
           placeholder="¿Qué sucedió en el día hoy?"
           minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
         />
       </Grid>
 
