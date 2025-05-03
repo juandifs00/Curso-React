aa2da98f7a67d1a3f490dd2e6490363f5cd74b20
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2025-05-02 22:37:28 -0500

    Test en los thunks auth

---
+++ 08-journal-app/tests/store/auth/thunks.test.js
@@ -0,0 +1,17 @@
+import { checkingCredentials } from "../../../src/store/auth/authSlice"
+import { checkingAuthentication } from "../../../src/store/auth/thunks"
+
+jest.mock('../../../src/firebase/providers')
+
+describe('Pruebas en AuthThunks', () => {
+
+    const dispatch = jest.fn()
+
+    beforeEach(() => jest.clearAllMocks())
+
+    test('Debe de invocar el checkingCredentials', async () => {
+
+        await checkingAuthentication()(dispatch);
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
+    })
+})
