c262dcf5ec72208ccd87e0981cb4e819bda2ae25
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2025-05-07 23:02:36 -0500

    Configuración final variables de entorno

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -6,29 +6,23 @@ import { getEnvironments } from "../helpers/getEnvironments";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// Dev/Prod
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDghcCxW9faBT99dtdZiNpYKKnzgrG4tNc",
-//   authDomain: "journal-app-b0611.firebaseapp.com",
-//   projectId: "journal-app-b0611",
-//   storageBucket: "journal-app-b0611.firebasestorage.app",
-//   messagingSenderId: "487209924895",
-//   appId: "1:487209924895:web:bdaff51327cea9d59a582a",
-// };
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
 
-// Your web app's Firebase configuration
-// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyBuf5NemWX3OUFJ3Mgswz_x0TresaN3xtU",
-  authDomain: "journalappbd-testing.firebaseapp.com",
-  projectId: "journalappbd-testing",
-  storageBucket: "journalappbd-testing.firebasestorage.app",
-  messagingSenderId: "370745253776",
-  appId: "1:370745253776:web:7169a58b339051613c1d15",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
-const env = getEnvironments;
-console.log(env);
 
 // Initialize Firebase
 export const Firebaseapp = initializeApp(firebaseConfig);
